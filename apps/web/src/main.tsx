05a8167d5b43a1585075000061da05342c219db6
Felix Ngyn <quangnguyen7403@gmail.com>
2025-12-24 23:15:34 +0700

    feat: add and config shadcn

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,9 +1,13 @@
 import { createRoot } from "react-dom/client";
 import "./global.css";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 const App = () => (
-  <div className="container bg-slate-200">
-    <h1 className="">Welcome to Huddle!</h1>
+  <div className="h-full bg-slate-200">
+    <h1 className="container">Welcome to Huddle!</h1>
+    <Input />
+    <Button>Click Me</Button>
   </div>
 );
 
