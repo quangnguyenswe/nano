475ea55d844de6c41f72d8324a8b0867fc33a0a9
Felix Ngyn <quangnguyen7403@gmail.com>
2026-01-12 22:00:06 +0700

    feat(common): initialize common package with TypeScript configuration, constants, and entry point

---
+++ packages/common/src/constants.ts
@@ -0,0 +1,14 @@
+export enum WS_EVENTS {
+  // SOCKET EVENTS
+  CONNECT = "connect",
+  DISCONNECT = "disconnect",
+  RECONNECT = "reconnect",
+
+  SEND_MESSAGE = "send-message",
+  NEW_MESSAGE = "new-message",
+
+  // Chat room events
+  JOIN_CHAT = "join-chat",
+  LEAVE_CHAT = "leave-chat",
+  
+}
