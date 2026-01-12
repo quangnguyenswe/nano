1c329218d17b1c9d94ea3fd0f6c4d1fc92dd8d27
Felix Ngyn <quangnguyen7403@gmail.com>
2026-01-12 22:00:43 +0700

    feat(email): update mailer to use environment variables for SMTP configuration

--- packages/email/src/mailer.ts
+++ packages/email/src/mailer.ts
@@ -4,6 +4,7 @@ import VerificationEmail, {
 } from "./templates/verification-email";
 import { render } from "@react-email/components";
 import dotenv from "dotenv";
+import "dotenv/config";
 
 dotenv.config();
 
@@ -11,11 +12,10 @@ const transporter = nodemailer.createTransport({
   service: "gmail", // Shortcut for Gmail's SMTP settings - see Well-Known Services
   auth: {
     type: "OAuth2",
-    user: "unknowhost96@gmail.com",
+    user: process.env.SMTP_FROM,
     clientId: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    refreshToken:
-      "1//04fk6E9pc384zCgYIARAAGAQSNwF-L9Ir-MZFzAI4vqsFnup5-zEPw9rFl9vF55s6Phs7pq7GYdNZzINCcoWKcL1Yz4dnQ16fTfE", // process.env.GOOGLE_REFRESH_TOKEN,
+    refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
   },
 });
 
@@ -24,11 +24,10 @@ export const sendVerificationEmail = async (
   subject: string,
   data: VerificationEmailProps,
 ) => {
-
   const emailTemplate = await render(VerificationEmail(data));
   try {
     await transporter.sendMail({
-      from: "Huddle <unknowhost96@gmail.com>",
+      from: process.env.SMTP_FROM,
       to,
       subject,
       html: emailTemplate,
